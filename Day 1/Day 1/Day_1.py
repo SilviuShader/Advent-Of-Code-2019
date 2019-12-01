03931241478ee2cf2c50840f8f01f19cef070ed9
SilviuShader <silviu.stancioiu@gmail.com>
2019-12-01 11:07:42 +0200

    Day 1

---
+++ Day 1/Day 1/Day_1.py
@@ -0,0 +1,38 @@
+
+def ModuleMass(x):   
+    result = 0
+    
+    while x > 0:
+        x = x // 3
+        x -= 2
+        if x >= 0:
+            result += x
+
+    return result
+
+def Solve1(messes):
+    result = 0
+
+    for mass in masses:
+        crtMass = mass // 3
+        crtMass -= 2
+        result += crtMass
+
+    return result
+
+def Solve2(masses):
+    result = 0
+
+    for mass in masses:
+        result += ModuleMass(mass)
+
+    return result
+
+inputFile = open("input.txt", "r")
+
+fileData = []
+fileData = inputFile.readlines()
+masses = [int(x) for x in fileData]
+
+print (Solve1(masses))
+print (Solve2(masses))
